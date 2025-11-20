e54a3ac967ccab32e8c5651401af45889a4af6e2
Esteban Forero <estebanmff@outlook.com>
2025-11-20 18:01:08 -0500

    updated

--- src/infra/database/mod.rs
+++ src/infra/database/mod.rs
@@ -1,4 +1,5 @@
-use sqlx::PgPool;
+use anyhow::{Context, Result};
+use sqlx::{PgPool, migrate::Migrator, postgres::PgPoolOptions};
 
 use crate::{
     domain::user::User,
@@ -10,7 +11,19 @@ struct PostgresDatabase {
     pool: PgPool,
 }
 
-impl PostgresDatabase {}
+impl PostgresDatabase {
+    async fn new(db_url: &str) -> Result<PostgresDatabase> {
+        let pool = PgPoolOptions::new().connect(db_url).await?;
+
+        Migrator::new(std::path::Path::new("./migrations"))
+            .await?
+            .run(&pool)
+            .await
+            .context("Failed to connect to postgres")?;
+
+        Ok(PostgresDatabase { pool })
+    }
+}
 
 impl UserDatabase for PostgresDatabase {
     async fn create_user(&self, user: User) -> UserDatabaseResult<()> {
