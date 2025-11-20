aaafe48d2abd54b5b465335a0d47c93c08c84c1d
soulsWTH <rustmaster@proton.me>
2025-11-20 18:57:09 -0500

    finished register

--- src/use_cases/auth_service.rs
+++ src/use_cases/auth_service.rs
@@ -1,4 +1,6 @@
-use bcrypt::{hash, DEFAULT_COST};
+use std::sync::Arc;
+
+use bcrypt::{DEFAULT_COST, hash};
 use chrono::{DateTime, Utc};
 use thiserror::Error;
 use uuid::Uuid;
@@ -7,27 +9,40 @@ use crate::{domain::user::User, use_cases::user_database::UserDatabase};
 
 type AuthResult<T> = Result<T, AuthError>;
 
-pub fn login(database: impl UserDatabase, username: String, password: String, jwt_secret: String) -> AuthResult<String> {}
+pub fn login(
+    database: Arc<impl UserDatabase>,
+    username: String,
+    password: String,
+    jwt_secret: String,
+) -> AuthResult<String> {
+    todo!()
+}
 
-pub fn register(database: impl UserDatabase, username: String, password: String, email: String) -> AuthResult<()> {
-    let encrypted_password = hash(password, DEFAULT_COST).map_err(|err| AuthError::PasswordHashingFailed(err.to_string()))?;
+pub fn register(
+    database: Arc<impl UserDatabase>,
+    username: String,
+    password: String,
+    email: String,
+) -> AuthResult<()> {
+    let encrypted_password = hash(password, DEFAULT_COST)
+        .map_err(|err| AuthError::PasswordHashingFailed(err.to_string()))?;
 
     let user = User {
         id: Uuid::new_v4(),
-        username: username,
-        email: email,
+        username,
+        email,
         password_hash: encrypted_password,
-        created_at: Utc::now();
+        created_at: Utc::now(),
+        updated_at: Utc::now(),
+    };
 
-    }
     database.create_user(user);
 
     Ok(())
 }
 
-
 #[derive(Error, Debug)]
 pub enum AuthError {
     #[error("Failed password hashing: {0}")]
-    PasswordHashingFailed(String)
+    PasswordHashingFailed(String),
 }
