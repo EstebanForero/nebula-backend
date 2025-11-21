48565c83e408d8b41c3c18ff5a1519f921d00e70
Esteban Forero <estebanmff@outlook.com>
2025-11-21 18:19:43 -0500

    rabbit mq development

---
+++ src/infra/rabbit_mq/mod.rs
@@ -0,0 +1,28 @@
+use amqprs::{
+    callbacks::{DefaultChannelCallback, DefaultConnectionCallback},
+    connection::{Connection, OpenConnectionArguments},
+};
+
+struct RabbitMQ {}
+
+impl RabbitMQ {
+    pub async fn new(host: &str, port: u16, username: &str, password: &str) -> RabbitMQ {
+        let mut args = OpenConnectionArguments::new("", port, username, password);
+        args.heartbeat(60);
+
+        let connection = Connection::open(&args)
+            .await
+            .expect("Error connecting to RabbitMQ");
+
+        connection
+            .register_callback(DefaultConnectionCallback)
+            .await?;
+
+        let channel = connection.open_channel(None).await?;
+        channel.register_callback(DefaultChannelCallback).await?;
+
+        // channel.confirm_select(args)
+
+        todo!()
+    }
+}
