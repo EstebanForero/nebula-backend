5bcd4729e4593fb9a64ea244de9088cbadd18f7f
Esteban Forero <estebanmff@outlook.com>
2025-11-22 12:48:13 -0500

    updated migration

--- migrations/20251122161952_web-notification-user.sql
+++ migrations/20251122161952_web-notification-user.sql
@@ -6,8 +6,3 @@ ALTER TABLE users
     ADD COLUMN webpush_p256dh   TEXT,
     ADD COLUMN webpush_auth     TEXT;
 
--- Down migration
-ALTER TABLE users
-    DROP COLUMN webpush_endpoint,
-    DROP COLUMN webpush_p256dh,
-    DROP COLUMN webpush_auth;
